24c7d0ce14b56ecb4bf023167ed4f3fd95e0e229
congibbs10 <congibbs10@gmail.com>
2023-01-18 17:36:52 -0700

    Change partition to alpine.

--- batch/tune_2019.sh
+++ batch/tune_2019.sh
@@ -3,10 +3,10 @@
 #SBATCH --job-name tune19
 #SBATCH --array=1-100
 #SBATCH --nodes=1
-#SBATCH --ntasks=1
+#SBATCH --ntasks=24
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=16G
-#SBATCH --partition shas
+#SBATCH --partition=amilan
 #SBATCH --time=24:00:00
 #SBATCH --qos normal
 #SBATCH --chdir=/projects/cgibbs10@colostate.edu/projs/BOCCRank/
