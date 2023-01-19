bf90ac619c2835c2785cc677a4542665ea2b3ad4
congibbs10 <congibbs10@gmail.com>
2023-01-18 19:05:08 -0700

    Lock file for updates.

--- inst/scripts/update_package.R
+++ inst/scripts/update_package.R
@@ -2,9 +2,5 @@
 options(echo = TRUE)
 
 print(getwd())
-pkgs <- setdiff(sort(unique(renv::dependencies()$Package)), c('ECoHeN', 'R', 'LPCTA'))
-install.packages(pkgs)
-if ('LPCTA' %in% installed.packages()[, 'Package']) {
-  remove.packages('LPCTA')
-}
+renv::install()
 devtools::install()
