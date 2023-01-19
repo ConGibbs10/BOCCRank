31c6eb87f50957b6be8d20179eac2f0801606854
congibbs10 <congibbs10@gmail.com>
2023-01-18 19:26:31 -0700

    Update package.

--- inst/scripts/update_package.R
+++ inst/scripts/update_package.R
@@ -2,8 +2,6 @@
 options(echo = TRUE)
 
 print(getwd())
-pkgs <- setdiff(sort(unique(renv::dependencies()$Package)), c('R', 'BOCCRank'))
-install.packages(pkgs)
 if ('BOCCRank' %in% installed.packages()[, 'Package']) {
   remove.packages('BOCCRank')
 }
